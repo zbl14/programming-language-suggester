4bac7890c3c3a1e198c930ea0cf874f9e3bcff37
zbl14 <ifthereisoneday@gmail.com>
2022-06-03 15:49:20 -0700

    add info about c#, JavaScript, Rust, Phython and Swift to the suggestion section

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $("#clearResult").click(function() {
-    $("#suggestionForUnderFive").hide();
-    $("#suggestion").hide();
-    $("#rubyInfo").hide();
+    $("#suggestionForUnderFive, #suggestion, #rubyInfo, #cSharpInfo, #javaScriptInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
   });
 
   $("#formOne").submit(function() {
@@ -25,31 +23,56 @@ $(document).ready(function() {
         $("#suggestedLang").text("Ruby");
         $(".clickable").click(function() {
           $("#rubyInfo").show();
+          $("#cSharpInfo, #javaScriptInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
         });
       } else if (ageInput < 20 && totalScore >= 10 && totalScore < 15) {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("C#");
+        $(".clickable").click(function() {
+          $("#cSharpInfo").show();
+          $("#rubyInfo, #javaScriptInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
+        });
       } else if (ageInput < 20 && totalScore >= 15) {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("JavaScript");
+        $(".clickable").click(function() {
+          $("#javaScriptInfo").show();
+          $("#rubyInfo, #cSharpInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
+        });
       } else if (ageInput >= 20 && totalScore < 10) {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("Ruby");
+        $(".clickable").click(function() {
+          $("#rubyInfo").show();
+          $("#cSharpInfo, #javaScriptInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
+        });
       } else if (ageInput >= 20 && totalScore >= 10 && totalScore < 15) {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("Rust");
+        $(".clickable").click(function() {
+          $("#rubyInfo").show();
+          $("#cSharpInfo, #javaScriptInfo, #rustInfo, #phythonInfo, #swiftInfo").hide();
+        });
       } else if (ageInput >= 20 && totalScore >= 15 && genderInput === "male") {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("Python");
+        $(".clickable").click(function() {
+          $("#phythonInfo").show();
+          $("#rubyInfo, #cSharpInfo, #javaScriptInfo, #rustInfo, #swiftInfo").hide();
+        });
       } else if (ageInput >= 20 && totalScore >= 15 && genderInput === "female") {
         $("#suggestion").show();
         $(".nameInput").text(nameInput);
         $("#suggestedLang").text("Swift");
+        $(".clickable").click(function() {
+          $("#swiftInfo").show();
+          $("#rubyInfo, #cSharpInfo, #javaScriptInfo, #rustInfo, #phythonInfo").hide();
+        });
       }
     } else if (ageInput > 0 && ageInput <= 5){
       $("#suggestion").hide();
