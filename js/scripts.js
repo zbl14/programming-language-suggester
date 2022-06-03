44def557282379a1c2540943e48ba958479f7c30
zbl14 <ifthereisoneday@gmail.com>
2022-06-03 10:13:07 -0700

    create branching for programming language suggestion based on the total score, gender and age

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
-    const personInput = $("input#name").val();
+    const nameInput = $("input#name").val();
     const ageInput = parseInt($("input#age").val());
     const genderInput =$("input#genderInput").val();
     const surveyOne = $("input:radio[name=survey]:checked").val();
@@ -9,7 +9,45 @@ $(document).ready(function() {
     const surveyThree = $("input:radio[name=survey]:checked").val();
     const surveyFour = $("input:radio[name=survey]:checked").val();
     const surveyFive = $("input:radio[name=survey]:checked").val();
+    const totalScore = surveyOne + surveyTwo + surveyThree + surveyFour +surveyFive
 
-    $(".surveyAnswer").text(surveyOne);
+    if (genderInput === 'male' || genderInput === 'female' && ageInput < 20 && ageInput >= 5) {
+      if (totalScore < 10) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("Ruby")
+        $(".surveyAnswer").text(totalScore);
+      } else if (totalScore >= 10 && totalScore < 15) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("C#")
+        $("#surveyAnswer").text(totalScore);
+      } else if (totalScore >= 15) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("Python")
+        $("#surveyAnswer").text(totalScore);
+      }
+    } else if (genderInput === 'male' || genderInput === 'female' && ageInput >= 20) {
+      if (totalScore < 10) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("Ruby")
+        $(".surveyAnswer").text(totalScore);
+      } else if (totalScore >= 10 && totalScore < 15) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("C#")
+        $("#surveyAnswer").text(totalScore);
+      } else if (totalScore >= 15) {
+        $("#suggestion").show()
+        $("#resultName").text(nameInput);
+        $("suggestedLang").text("Python")
+        $("#surveyAnswer").text(totalScore);
+      }
+    }
+
+    
+   
   });
 });
