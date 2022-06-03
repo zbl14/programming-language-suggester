f6140d80ac01bdfc29ff4b8478ceb328c0767172
zbl14 <ifthereisoneday@gmail.com>
2022-06-03 09:32:48 -0700

    add form for collecting for favSports, favBerverage, sleeping time and favTypeOfMovies

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     const personInput = $("input#name").val();
-    const addressInput = $("input#address").val();
-    const survey = $("input:radio[name=survey]:checked").val();
+    const ageInput = parseInt($("input#age").val());
+    const genderInput =$("input#genderInput").val();
+    const surveyOne = $("input:radio[name=survey]:checked").val();
+    const surveyTwo = $("input:radio[name=survey]:checked").val();
+    const surveyThree = $("input:radio[name=survey]:checked").val();
+    const surveyFour = $("input:radio[name=survey]:checked").val();
+    const surveyFive = $("input:radio[name=survey]:checked").val();
 
-    $(".surveyAnswer").text(survey);
+    $(".surveyAnswer").text(surveyOne);
   });
 });
